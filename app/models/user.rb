bd680d1d6c8996dacda2afe4c013be9d807606fe
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-03-05 13:07:29 +0900

    エラーメッセージが重複表示されない修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
   validates :birthday, presence: true
 
   with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers'} do
-    validates :password, presence: true
+    validates :password, presence: false
   end
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
     validates :first_name, presence: true
     validates :last_name, presence: true
   end
-  
+
   with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
     validates :last_name_read, presence: true
     validates :first_name_read, presence: true
