2078566a4e5aef2186774c0b687f61a37e48146e
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-03-05 12:45:40 +0900

    with_optionを使用してのバリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :last_name_read, presence: true
-  validates :first_name, presence: true
-  validates :first_name_read, presence: true
   validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers'} do
+    validates :password, presence: true
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
+    validates :last_name_read, presence: true
+    validates :first_name_read, presence: true
+  end
 end
