bbb6b094d2d97e19e805f3147a7561d3a6213106
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-03-05 11:02:11 +0900

    新規登録機能とログインの有無でビューが変わる機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auto
+  before_action :configure_permitted_parameters, if: :device_controller?
 
   private
   def basic_auto
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_params_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :last_name_read, :first_name, :first_name_read, :birthday])
   end
 end
