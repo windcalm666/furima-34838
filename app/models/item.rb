901925c3b6d354a7735622b40698d8718818d442
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-03-08 12:23:08 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,22 @@
 class Item < ApplicationRecord
-
   validates :item
   validates :text
   validates :price
-  validates :category_id
-  validates :status_id
-  validates :charge_id
-  validates :prefecture_id
-  validates :days_id
+
+  with_options presence: true, numericality: { other_than: 1 }  do
+    validates :category_id
+    validates :status_id
+    validates :charge_id
+    validates :prefecture_id
+    validates :days_id
+  end
 
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :day
 end
