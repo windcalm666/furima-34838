1cdf2aad64e9f67b70e0c8625e183bdce46cbe4f
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-03-09 19:01:13 +0900

    Revert "出品した本人にのみ編集機能の実装"
    This reverts commit 84cccdacf8d4b7b65a82dbb1307afe54cf59c5ce.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :find_item, only: [:show, :edit, :update]
-  
+ 
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -20,29 +19,11 @@ class ItemsController < ApplicationController
   end
 
   def show
-  end
-
-  def edit
-    unless current_user.id == @item.user.id
-      redirect_to root_path
-    end
-  end
-
-  def update
-    @item.update(item_params)
-    if @item.valid?
-      redirect_to item_path(@item.id)
-    else
-      render :edit
-    end
+    @item = Item.find(params[:id])
   end
 
   private
   def item_params
     params.require(:item).permit(:item, :text, :price, :category_id, :status_id, :charge_id, :day_id, :prefecture_id, :image).merge(user_id: current_user.id)
-  end
-
-  def find_item
-    @item = Item.find(params[:id])
   end
 end
