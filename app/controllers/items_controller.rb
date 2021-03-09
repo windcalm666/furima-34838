e533c37ae281685378247baeb746d926cbd3befa
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-03-09 19:38:59 +0900

    メソッドにまとめる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
- 
+  before_action :find_item, only: [:show, :destroy]
+
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -19,13 +20,11 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if current_user.id == item.user.id
-      item.destroy
+    if current_user.id == @item.user.id
+      @item.destroy
     end
     redirect_to root_path
   end
@@ -33,5 +32,9 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:item, :text, :price, :category_id, :status_id, :charge_id, :day_id, :prefecture_id, :image).merge(user_id: current_user.id)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 end
