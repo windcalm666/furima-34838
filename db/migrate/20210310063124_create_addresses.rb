8c4c406aecaa4674220ede25de051f8b0e9b7783
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-03-10 17:56:35 +0900

    各モデルにnull: false追加

--- db/migrate/20210310063124_create_addresses.rb
+++ db/migrate/20210310063124_create_addresses.rb
@@ -7,7 +7,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string :address, null: false
       t.string :building_name
       t.string :phone_number, null: false
-      t.references :order, foreign_key: true 
+      t.references :order, null: false, foreign_key: true 
       t.timestamps
     end
   end
