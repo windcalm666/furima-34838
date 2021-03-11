779a8d33833998928c3c6a1f91faf9ece938a202
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-03-11 19:23:50 +0900

    モデル単体テストの実装

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :order_address do
-    
+    postal_cord { "123-4567" }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    city { '東京都' }
+    address {'1-1'}
+    building_name { '青山ビルディング' }
+    phone_number { "09012345678" }
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
